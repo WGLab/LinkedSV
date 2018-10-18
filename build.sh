3afb29c28b2d96919db6dfb300e576db5311f3c3
Li Fang <fangli0813@gmail.com>
2018-10-18 00:29:26 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+cd htslib-1.3/
+make
+make check
+cp libhts.a ../lib/
+cd .. 
 gcc  -g -O2 -I ./include  -L ./lib/ -o extract_barcode_info extract_barcode_info.c  -l hts -l z -l m -l pthread 
 gcc  -g -O2 -I ./include  -L ./lib/ -o output_bam_coreinfo  output_bam_coreinfo.c   -l hts -l z -l m -l pthread 
 gcc  -g -O2 -std=gnu99              -o remove_sparse_nodes  remove_sparse_nodes.c  tk.c
+
