2fc4b7470efba93a7ba69c506d1ed88507c0495d
Li Fang <fangli0813@gmail.com>
2019-04-04 03:16:42 -0400

    add visualization tools

--- scripts/plot_2d_barcodes.py
+++ scripts/plot_2d_barcodes.py
@@ -8,6 +8,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import my_utils
